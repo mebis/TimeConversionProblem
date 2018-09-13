c8ea808fea34d6864746498d2229a54f99bd6e8c
Masa Ebisawa <ebisawa.masaharu@idsi.com>
2018-09-13 03:59:51 -0400

    checked the validity of the input date by writing tests for each case.

--- src/main/java/com/masa/tcp/service/Service.java
+++ src/main/java/com/masa/tcp/service/Service.java
@@ -3,6 +3,7 @@ package com.masa.tcp.service;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,9 +23,19 @@ public class Service {
 	
 	public String convertTime(String time) throws ParseException {
 		
+		if(!checkValidTimeFormat(time)) {
+			throw new ParseException("", 0);
+		}
+		
 		Date date = input.parse(time);
 		
 		return output.format(date);
+	}
+	
+	private boolean checkValidTimeFormat(String time) {
+		String regexFormat = "^(0[0-9]|1?[0-2]):[0-5][0-9]:[0-5][0-9][A|P]M$";
+		
+		return Pattern.matches(regexFormat, time);
 	}
 
 }
--- src/test/java/com/masa/tcp/service/TestTimeConversion.java
+++ src/test/java/com/masa/tcp/service/TestTimeConversion.java
@@ -5,6 +5,8 @@ import java.text.ParseException;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +14,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class TestTimeConversion {
+	
+	Logger logger = LoggerFactory.getLogger(TestTimeConversion.class);
 
 	@TestConfiguration
     static class TimeConversionProblemImplTestContextConfiguration {
@@ -26,17 +30,43 @@ public class TestTimeConversion {
     private Service service;
 	
 	@Test
-	public void test() throws ParseException {
-		
+	public void testValid() throws ParseException {
 		String date = null;
 		
-		if(service != null) {
-			date = service.convertTime("10:43:57PM");
-		}
+		date = service.convertTime("12:00:00AM");
+		Assert.assertTrue("00:00:00".equals(date));
 		
-		System.out.println("DATE is: "+date);
+		logger.info("12:00:00AM is: "+date);
 		
-		Assert.assertTrue("22:43:57".equals(date));
+		date = service.convertTime("12:00:00PM");
+		Assert.assertTrue("12:00:00".equals(date));
+		
+		logger.info("12:00:00PM is: "+date);
+		
+		date = service.convertTime("07:05:45PM");
+		Assert.assertTrue("19:05:45".equals(date));
+		
+		logger.info("07:05:45PM is: "+date);
+	}
+	
+	@Test(expected = ParseException.class)
+	public void testInvalidHour() throws ParseException {
+		service.convertTime("13:00:00AM");
+	}
+	
+	@Test(expected = ParseException.class)
+	public void testInvalidMinute() throws ParseException {
+		service.convertTime("11:66:00AM");
+	}
+	
+	@Test(expected = ParseException.class)
+	public void testInvalidSeconds() throws ParseException {
+		service.convertTime("11:33:77AM");
+	}
+	
+	@Test(expected = ParseException.class)
+	public void testInvalidAMPM() throws ParseException {
+		service.convertTime("11:33:77SS");
 	}
 
 }
